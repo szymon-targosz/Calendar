7235e7c8e37cce7795eb3a2ad91db8870595bdab
Szymon Targosz <szymon7.t@gmail.com>
2017-10-04 15:22:02 +0200

    remove item from object

--- js/app.js
+++ js/app.js
@@ -6,18 +6,17 @@ class Calendar{
         this.currentYear = currentDate.getFullYear();
         this.daysNames = ['Sun', 'Mon','Tue','Wed','Thu','Fri', 'Sat'];
         this.monthsNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        this.tasksList = {};
+        this.tasksList = {11102017:['mleko', 'jajka'], 23112017: ['pomidory', 'ziemniaki']};
+        this.td = null;
     }
 
     next(){
-        console.log(this.currentMonth);
         if(this.currentMonth === 11){
             this.currentYear += 1;
             this.currentMonth = 0;
         } else{
             this.currentMonth += 1;
         };
-        console.log(this.currentMonth);
         this.render();
     }
 
@@ -33,17 +32,49 @@ class Calendar{
 
     buttons(){
         const self = this;
+
         document.getElementById('prev').addEventListener('click', function(){
             self.prev();
         });
         document.getElementById('next').addEventListener('click', function(){
             self.next();
         });
+
+        let list = document.querySelector('ol');
+
+        list.addEventListener('click', function(event) {
+
+            if(event.target.className === 'delete'){
+                // remove item from objecy this.tasksList
+                let thisTask = '';
+                const thisTaskDate = document.querySelector('#task header p').innerHTML;
+
+                for(let i = 0; i < event.target.parentElement.innerHTML.indexOf('<'); i++){
+                    thisTask += event.target.parentElement.innerHTML[i];
+                };
+                const thisTaskDateCheck = thisTaskDate.replace(/\./g, '');
+
+                const indexOfTheTask = self.tasksList[`${thisTaskDateCheck}`].indexOf(thisTask);
+                self.tasksList[`${thisTaskDateCheck}`].splice(indexOfTheTask, 1)
+
+                // remove item from list
+                event.target.parentElement.remove();
+
+            } else if (event.target.className === 'done'){
+
+                if(event.target.parentElement.style.textDecoration != 'line-through'){
+                    event.target.parentElement.style.textDecoration = 'line-through';
+                } else{
+                    event.target.parentElement.style.textDecoration = 'none';
+                };
+            };
+        });
     }
 
     render(){
         this.createCalendar(this.currentYear, this.currentMonth);
         this.tasks();
+        this.mark();
     }
 
     createCalendar(year, month){
@@ -55,7 +86,9 @@ class Calendar{
         } else{
             endOfPrev = new Date(year - 1, 11, 0).getDate();
         };
+
         document.querySelector('p').innerHTML = `${this.monthsNames[month]} ${year}`;
+
         let calendarTable = '<table><thead><tr>';
         for(let i = 0; i < this.daysNames.length; i++){
             calendarTable += '<th>' + this.daysNames[i] + '</th>'
@@ -79,6 +112,7 @@ class Calendar{
             const control = new Date();
             const controlMonth = control.getMonth();
             const controlYear = control.getFullYear();
+
             if(controlYear === this.currentYear && controlMonth === this.currentMonth && i === this.currentDay){
                 calendarTable += '<td id="now">' + i + '</td>';
               } else{
@@ -96,26 +130,35 @@ class Calendar{
             };
             i++;
         } while(i <= end);
-        calendarTable += '</tbody></table>';
-        document.getElementById('calendarTable').innerHTML = calendarTable;
 
+        calendarTable += '</tbody></table>';
+        // display calendar
+        document.getElementById('calendarTable').innerHTML = calendarTable;
+    }
+
+    mark(){
+        console.log(this.tasksList);
+        const keys = Object.keys(this.tasksList);
+        console.log(keys);
     }
 
     tasks(){
         const self = this;
         const tds = document.querySelectorAll('td:not(.otherMonth)');
         tds.forEach(function(elem){
-            elem.addEventListener("click", showTaskElem);
+            elem.addEventListener('click', showTaskElem);
         });
 
         function showTaskElem(e){
-            console.log(this);
+            self.td = this;
+
             const choosen = document.querySelector('.choosen');
             if(choosen != null){
+                document.querySelector('.choosen').style.color = '#E0E0E0';
                 document.querySelector('.choosen').classList.remove('choosen');
             };
             document.getElementById('task').style.display = 'block';
-
+            this.style.color = '#E50000';
 
             let date = '';
             if(Number.parseInt(this.innerHTML) < 10){
@@ -133,8 +176,8 @@ class Calendar{
             date += self.currentYear;
 
             const dataCheck = date.replace(/\./g, '');
-
             document.querySelector('#task p').innerHTML = date;
+
             this.classList.add('choosen');
 
             // reset list and show tasks from this.tasksList
@@ -175,44 +218,54 @@ class Calendar{
 
     addTask(){
         const self = this;
-        document.querySelector('#addBtn').addEventListener('click', addTask);
+        const addBtn = document.querySelector('#addBtn');
+
+        addBtn.addEventListener('click', addTask);
+
         function addTask(e){
-            console.log('addTask func');
             const inputValue = document.querySelector('input').value;
 
-            // update this.tasksList object - new data
-            const taskDate = document.querySelector('#task p').innerHTML;
-            const taskDateChanged = taskDate.replace(/\./g, '');
-            if(self.tasksList.hasOwnProperty(taskDateChanged)){
-                self.tasksList[`${taskDateChanged}`].push(inputValue);
-            } else {
-                self.tasksList.x = [inputValue];
-                let str = null;
-                str = JSON.stringify(self.tasksList);
-                str = str.replace('x', taskDateChanged);
-                self.tasksList = JSON.parse(str)
+            if(inputValue === ''){
+                document.querySelector('input').setAttribute('placeholder', 'You have to type something');
+            } else{
+                document.querySelector('input').setAttribute('placeholder', 'Add task...');
+
+                // update this.tasksList object - new data
+                const taskDate = document.querySelector('#task p').innerHTML;
+                const taskDateChanged = taskDate.replace(/\./g, '');
+
+                if(self.tasksList.hasOwnProperty(taskDateChanged)){
+                    self.tasksList[`${taskDateChanged}`].push(inputValue);
+                } else {
+                    self.tasksList.x = [inputValue];
+                    let str = null;
+                    str = JSON.stringify(self.tasksList);
+                    str = str.replace('x', taskDateChanged);
+                    self.tasksList = JSON.parse(str)
+                };
+
+                // new li element
+                const newLi = document.createElement('li');
+                const delBtn = document.createElement('button');
+                delBtn.classList.add('delete');
+                delBtn.innerHTML = 'delete';
+
+                const doneBtn = document.createElement('button');
+                doneBtn.classList.add('done');
+                doneBtn.innerHTML = 'done';
+
+                newLi.innerHTML = inputValue;
+                newLi.appendChild(delBtn);
+                newLi.appendChild(doneBtn);
+                document.querySelector('ol').appendChild(newLi);
+
+                // clear input
+                document.querySelector('input').value = '';
+
+                self.mark();
             };
-            console.log(self.tasksList);
-
-            // new li element
-            const newLi = document.createElement('li');
-            const delBtn = document.createElement('button');
-            delBtn.classList.add('delete');
-            delBtn.innerHTML = 'delete';
-
-            const doneBtn = document.createElement('button');
-            doneBtn.classList.add('done');
-            doneBtn.innerHTML = 'done';
-
-            newLi.innerHTML = inputValue;
-            newLi.appendChild(delBtn);
-            newLi.appendChild(doneBtn);
-            document.querySelector('ol').appendChild(newLi);
-            document.querySelector('input').value = '';
         }
     }
-
-
 }
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -220,5 +273,4 @@ document.addEventListener('DOMContentLoaded', function(){
     test.render();
     test.buttons();
     test.addTask();
-
 });
